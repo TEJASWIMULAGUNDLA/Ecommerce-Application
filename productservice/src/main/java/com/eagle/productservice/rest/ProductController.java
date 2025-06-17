7d543ab28f8f19e8f0bdeb9cd75bbd2a20743b5c
Dev2 <Dev2@gmail.com>
2025-06-17 22:55:52 +0530

    change entity and add controller and dto package and class

--- productservice/src/main/java/com/eagle/productservice/rest/ProductController.java
+++ productservice/src/main/java/com/eagle/productservice/rest/ProductController.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/product-api")
 public class ProductController {
 
-    publci ResponseEntity<String> createProdoct()
+
 }
