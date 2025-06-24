b61efe80c69a454317314f3cd3dbe472c8cdcec2
Dev2 <Dev2@gmail.com>
2025-06-24 18:52:30 +0530

    change in service for setting stock quantity

--- productservice/src/main/java/com/eagle/productservice/controller/ProductController.java
+++ productservice/src/main/java/com/eagle/productservice/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.eagle.productservice.dto.ProductDto;
 import com.eagle.productservice.dto.ProductRequestDTO;
 import com.eagle.productservice.dto.ProductResponseDTO;
 import com.eagle.productservice.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
@@ -13,9 +14,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api/product")
+@Slf4j
 public class ProductController {
 
     @Autowired
@@ -28,8 +31,9 @@ public class ProductController {
            @RequestPart(value = "image", required = false) MultipartFile imageFile,
            @RequestPart("productDTO") ProductDto updatedProductDto
            ) throws IOException {
-
+    log.info("Updating product with ID: {}", id);
        ProductDto updated = productService.updateProduct(id, updatedProductDto, imageFile);
+       log.info("Product updated successfully with prodId: {} | productName : {}", updated.getProdId(), updated.getProductName());
        return ResponseEntity.ok(updated);
    }
 
